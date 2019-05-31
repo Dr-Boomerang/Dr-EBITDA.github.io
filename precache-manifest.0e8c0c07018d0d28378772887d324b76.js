8a7ac9046211853b73ccdacc47a7398788861d31
Elie <kotlarsky.elie@gmail.com>
2019-05-31 17:23:32 +0300

    first commit

---
+++ precache-manifest.0e8c0c07018d0d28378772887d324b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ce11f44ec4d05b5ee7a9a859a1c608",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3715f50aa9032219ff0e",
+    "url": "/static/css/main.99d4623f.chunk.css"
+  },
+  {
+    "revision": "590b4c51f03b78a8fb07",
+    "url": "/static/js/2.a16a7e05.chunk.js"
+  },
+  {
+    "revision": "3715f50aa9032219ff0e",
+    "url": "/static/js/main.06131b0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
